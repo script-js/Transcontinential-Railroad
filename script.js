7906fcdc13a18cc420c28f7c0b3d61bb6afd59ec
script-js <134544514+script-js@users.noreply.github.com>
2024-05-28 17:39:00 -0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,19 @@
+function popout(text) {
+  if (!text) {
+    popup.style.height = "2px"
+    popup.style.width = "2px"
+    popup.innerHTML = "";
+    setTimeout(function() {
+      popupBack.style.display = "none";
+    },400)
+  } else {
+    popupBack.style.display = "flex";
+    setTimeout(function() {
+      popup.style.height = "90%"
+      popup.style.width = "90%"
+    },50)
+    setTimeout(function() {
+      popup.innerHTML = text;
+    },400)
+  }
+}
