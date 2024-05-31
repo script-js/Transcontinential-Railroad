4ed94e294dc7b4738a2e67ec904d3da390328c5c
script-js <134544514+script-js@users.noreply.github.com>
2024-05-31 08:07:14 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Encounters: raid, buffalo grounds, smaller land
 
-var points = [Math.ceil(Math.random() * (38 - 20) + 20),Math.ceil(Math.random() * (56 - 39) + 39),Math.ceil(Math.random() * (75 - 57) + 57),Math.ceil(Math.random() * (88 - 76) + 76)]
+var points = [Math.ceil(Math.random() * (38 - 20) + 20),Math.ceil(Math.random() * (56 - 39) + 39),Math.ceil(Math.random() * (75 - 57) + 57),Math.ceil(Math.random() * (87 - 76) + 76)]
 
 var checkpoints = [
   {percent: points[0],message: "test<br><button onclick='cAction(negative)'>negative</button><button onclick='cAction(positive)'>positive</button>"},
